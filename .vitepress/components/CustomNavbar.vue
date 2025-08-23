a1940c4306c0e97b719ef4d627c91b2f828b111c
Yufan Han <larryivanhan@gmail.com>
2025-08-23 23:29:49 +0800

    test: Changed the logo.

--- .vitepress/components/CustomNavbar.vue
+++ .vitepress/components/CustomNavbar.vue
@@ -3,7 +3,8 @@
     <div class="container">
       <a href="/fudan" class="title">
         <!-- Add Logo SVG or Image here if available -->
-        <img src="https://ooo.0x0.ooo/2025/04/22/OczS3F.png" style="height: 40px" />
+        <img src="https://static.igem.wiki/teams/5643/img/screenshot-2025-08-23-at-23-28-02.webp" style="height: 50px" />
+        <span class="title-text">Fudan</span>
       </a>
       <!-- Desktop Navigation -->
       <div class="nav-links-desktop">
