950023394782bb794399cfb953175e7a46adf4c5
Yufan Han <larryivanhan@gmail.com>
2025-08-26 10:14:07 +0800

    fix: Homepage error.

--- .vitepress/components/HomePage.vue
+++ .vitepress/components/HomePage.vue
@@ -2,12 +2,6 @@
   <div class="home-page">
     <h1>Welcome to Our Wiki</h1>
     <!-- Add your home page content here -->
-    <ShinyText 
-    text="Just some shiny text!" 
-    :disabled="false" 
-    :speed="8" 
-    class-name="your-custom-class"
-  />
   </div>
 </template>
 
